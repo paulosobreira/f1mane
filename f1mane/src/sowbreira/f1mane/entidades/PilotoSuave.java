a0fa075e8f53d8c1836bcdfe772e39add9d3b483
sowbreira <sowbreira@gmail.com>
2016-05-05 13:47:32 -0300

    -Interface de atualização suave

---
+++ f1mane/src/sowbreira/f1mane/entidades/PilotoSuave.java
@@ -0,0 +1,15 @@
+package sowbreira.f1mane.entidades;
+
+public interface PilotoSuave {
+
+	public No getNoAtual();
+
+	public No getNoAtualSuave();
+
+	public int getGanhoSuave();
+
+	public void setGanhoSuave(int ganhoSuave);
+
+	public void setNoAtualSuave(No noAtualSuave);
+
+}
